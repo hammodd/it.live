ce53c6794ea8b26e06624c52fa43d1384ee8a61d
hammodd <101920360+hammodd@users.noreply.github.com>
2022-11-11 22:06:34 +0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,21 @@
+﻿(function ($) {
+    $(document).ready(function () {
+        photopile.scatter($('#jsn-themepile-wrapper-' + 'J0KEf'), {
+            thumbOverlap: 50,
+            thumbRotation: 45,
+            thumbBorderWidth: 2,
+            thumbBorderColor: '#ffffff',
+            thumbBorderHover: '#ffffff',
+            thumbShadow: true,
+            thumbShadowColor: '#ffffff',
+            thumbWidth: 130,
+            thumbHeight: 130,
+            fadeDuration: 200,
+            pickupDuration: 500,
+            clickAction: 'show_original_image',
+            openLinkIn: 'current_browser',
+            classContainer: 'J0KEf',
+            rootURL: '',
+        });  // ### initialize the photopile ###
+    });
+})(jsnThemePilejQuery);
